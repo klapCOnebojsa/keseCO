ebab61383fb9e6db24a4b2a3cf1cef9b5077556d
klap <klap@hp>
2015-07-05 14:31:51 +0200

    poruka71_0001

--- src/Class/Povezivanje/Procitaj.java
+++ src/Class/Povezivanje/Procitaj.java
@@ -104,5 +104,15 @@ public class Procitaj {
     }
     public String ProcitajSifruMargina(BrokerDAO brokerDAO) throws Exception {
         return brokerDAO.ProcitajSifruMargina();
-    }     
+    } 
+    
+    
+    
+        
+        
+        
+        
+    public void dd(){
+       String hh = "71_0002";
+    }
 }
